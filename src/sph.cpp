4a4dc3f357f66048e6966f43be91bd60565187fc
ovidiom <ovidiom@conguito>
2011-01-15 11:30:31 +0100

    - some minor code clean up

--- src/sph.cpp
+++ src/sph.cpp
@@ -18,92 +18,90 @@ GridElement *grid;
 GridElement *sleeping_grid;
 
 
-inline float kernel(const Vector3f &r, const float h)
-{
-	if (length(r) > h)
-		return (0.0f);
+inline float kernel(const Vector3f &r, const float h) {
+	if (length(r) > h) {
+		return 0.0f;
+	}
 
-	return (315.0f / (64.0f * PI_FLOAT * POW9(h)) * CUBE(SQR(h) - dot(r, r)));
+	return 315.0f / (64.0f * PI_FLOAT * POW9(h)) * CUBE(SQR(h) - dot(r, r));
 }
 
-inline Vector3f gradient_kernel(const Vector3f &r, const float h)
-{
-	if (length(r) > h)
-		return (Vector3f(0.0f, 0.0f, 0.0f));
+inline Vector3f gradient_kernel(const Vector3f &r, const float h) {
+	if (length(r) > h) {
+		return Vector3f(0.0f, 0.0f, 0.0f);
+	}
 
-	return (-945.0f / (32.0f * PI_FLOAT * POW9(h)) * SQR(SQR(h) - dot(r, r)) * r);
+	return -945.0f / (32.0f * PI_FLOAT * POW9(h)) * SQR(SQR(h) - dot(r, r)) * r;
 }
 
-inline float laplacian_kernel(const Vector3f &r, const float h)
-{
-	if (length(r) > h)
-		return (0.0f);
+inline float laplacian_kernel(const Vector3f &r, const float h){
+	if (length(r) > h) {
+		return 0.0f;
+	}
 
-	return (  945.0f / (32.0f * PI_FLOAT * POW9(h))
-	        * (SQR(h) - dot(r, r)) * (7.0f * dot(r, r) - 3.0f * SQR(h)));
+	return   945.0f / (32.0f * PI_FLOAT * POW9(h))
+	       * (SQR(h) - dot(r, r)) * (7.0f * dot(r, r) - 3.0f * SQR(h));
 }
 
-inline Vector3f gradient_pressure_kernel(const Vector3f &r, const float h)
-{
-	if ((length(r) > h) || (length(r) < 0.001f))
-		return (Vector3f(0.0f, 0.0f, 0.0f));
+inline Vector3f gradient_pressure_kernel(const Vector3f &r, const float h) {
+	if ((length(r) > h) || (length(r) < 0.001f)) {
+		return Vector3f(0.0f, 0.0f, 0.0f);
+	}
 
 	return (-45.0f / (PI_FLOAT * POW6(h)) * SQR(h - length(r)) * normalize(r));
 }
 
-inline float laplacian_viscosity_kernel(const Vector3f &r, const float h)
-{
-	if (length(r) > h)
-		return (0.0f);
+inline float laplacian_viscosity_kernel(const Vector3f &r, const float h) {
+	if (length(r) > h) {
+		return 0.0f;
+	}
 
 	return (45.0f / (PI_FLOAT * POW6(h)) * (h - length(r)));
 }
 
-inline void add_density(Particle &particle, Particle &neighbour)
-{
+inline void add_density(Particle &particle, Particle &neighbour) {
 	Vector3f r = particle.position - neighbour.position;
 	particle.density += neighbour.mass * kernel(r, core_radius);
 }
 
-void sum_density(GridElement &grid_element, Particle &particle)
-{
+void sum_density(GridElement &grid_element, Particle &particle) {
 	list<Particle> &plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		add_density(particle, *piter);
+	}
 }
 
-inline void sum_all_density(int i, int j, int k, Particle &particle)
-{
+inline void sum_all_density(int i, int j, int k, Particle &particle) {
 	particle.density = 0.0f;
 
-	for (int x = i - 1; x <= i + 1; x++)
-		for (int y = j - 1; y <= j + 1; y++)
-			for (int z = k - 1; z <= k + 1; z++)
-			{
-				if ((x < 0) || (x >= GRID_WIDTH))
+	for (int x = i - 1; x <= i + 1; x++) {
+		for (int y = j - 1; y <= j + 1; y++) {
+			for (int z = k - 1; z <= k + 1; z++) {
+				if (   (x < 0) || (x >= GRID_WIDTH)
+					|| (y < 0) || (y >= GRID_HEIGHT)
+					|| (z < 0) || (z >= GRID_DEPTH)) {
 					continue;
-				if ((y < 0) || (y >= GRID_HEIGHT))
-					continue;
-				if ((z < 0) || (z >= GRID_DEPTH))
-					continue;
+				}
 
 				sum_density(GRID(x, y, z), particle);
 			}
+		}
+	}
 }
 
-void update_density(int i, int j, int k)
-{
+void update_densities(int i, int j, int k) {
 	GridElement &grid_element = GRID(i, j, k);
 
 	list<Particle> &plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		sum_all_density(i, j, k, *piter);
+	}
 }
 
-inline void add_forces(Particle &particle, Particle &neighbour)
-{
-	if (&particle == &neighbour)
+inline void add_forces(Particle &particle, Particle &neighbour) {
+	if (&particle == &neighbour) {
 		return;
+	}
 
 	Vector3f r = particle.position - neighbour.position;
 
@@ -127,46 +125,43 @@ inline void add_forces(Particle &particle, Particle &neighbour)
 	particle.color_laplacian += neighbour.mass / neighbour.density * value;
 }
 
-void sum_forces(GridElement &grid_element, Particle &particle)
-{
+void sum_forces(GridElement &grid_element, Particle &particle) {
 	list<Particle>  &plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		add_forces(particle, *piter);
+	}
 }
 
-void sum_all_forces(int i, int j, int k, Particle &particle)
-{
+void sum_all_forces(int i, int j, int k, Particle &particle) {
 	particle.force = Vector3f(0.0f, 0.0f, 0.0f);
 	particle.color_gradient = Vector3f(0.0f, 0.0f, 0.0f);
 	particle.color_laplacian = 0.0f;
 
-	for (int x = i - 1; x <= i + 1; x++)
-		for (int y = j - 1; y <= j + 1; y++)
-			for (int z = k - 1; z <= k + 1; z++)
-			{
-				if ((x < 0) || (x >= GRID_WIDTH))
+	for (int x = i - 1; x <= i + 1; x++) {
+		for (int y = j - 1; y <= j + 1; y++) {
+			for (int z = k - 1; z <= k + 1; z++) {
+				if (   (x < 0) || (x >= GRID_WIDTH)
+					|| (y < 0) || (y >= GRID_HEIGHT)
+					|| (z < 0) || (z >= GRID_DEPTH)) {
 					continue;
-				if ((y < 0) || (y >= GRID_HEIGHT))
-					continue;
-				if ((z < 0) || (z >= GRID_DEPTH))
-					continue;
+				}
 
 				sum_forces(GRID(x, y, z), particle);
 			}
+		}
+	}
 }
 
-void update_forces(int i, int j, int k)
-{
+void update_forces(int i, int j, int k) {
 	GridElement &grid_element = GRID(i, j, k);
 	list<Particle>&plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		sum_all_forces(i, j, k, *piter);
+	}
 }
 
-inline void update_velocity(Particle &particle)
-{
-	if (length(particle.color_gradient) > 0.001f)
-	{
+inline void update_velocity(Particle &particle) {
+	if (length(particle.color_gradient) > 0.001f) {
 		particle.force +=   -sigma * particle.color_laplacian
 		                  * normalize(particle.color_gradient);
 	}
@@ -177,82 +172,98 @@ inline void update_velocity(Particle &particle)
 	particle.velocity += timestep * acceleration;
 }
 
-inline void update_position(Particle &particle)
-{
+inline void update_position(Particle &particle) {
 	particle.position += timestep * particle.velocity;
 }
 
-void update_particles(int i, int j, int k)
-{
+void update_particles(int i, int j, int k) {
 	GridElement &grid_element = GRID(i, j, k);
 
 	list<Particle> &plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
-	{
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		update_velocity(*piter);
 		update_position(*piter);
 	}
 }
 
-inline void insert_into_grid(int i, int j, int k)
-{
+inline void insert_into_grid(int i, int j, int k) {
 	GridElement &grid_element = GRID(i, j, k);
 
 	list<Particle> &plist = grid_element.particles;
-	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++)
+	for (list<Particle>::iterator piter = plist.begin(); piter != plist.end(); piter++) {
 		ADD_TO_GRID(sleeping_grid, *piter);
+	}
 }
 
-void update_grid()
-{
-	for (int i = 0; i < GRID_WIDTH; i++)
-		for (int j = 0; j < GRID_HEIGHT; j++)
-			for (int k = 0; k < GRID_DEPTH; k++)
-			{
+void update_grid() {
+	for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			for (int k = 0; k < GRID_DEPTH; k++) {
 				insert_into_grid(i, j, k);
 				GRID(i, j, k).particles.clear();
 			}
+		}
+	}
 
 	/* Swap the grids. */
 	swap(grid, sleeping_grid);
 }
 
-void update(void(*inter_hook)() = NULL, void(*post_hook)() = NULL)
-{
-	int i, j, k;
+void update_densities() {
+    for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			for (int k = 0; k < GRID_DEPTH; k++) {
+				update_densities(i, j, k);
+			}
+		}
+	}
+}
 
-	for (i = 0; i < GRID_WIDTH; i++)
-		for (j = 0; j < GRID_HEIGHT; j++)
-			for (k = 0; k < GRID_DEPTH; k++)
-				update_density(i, j, k);
-
-	for (i = 0; i < GRID_WIDTH; i++)
-		for (j = 0; j < GRID_HEIGHT; j++)
-			for (k = 0; k < GRID_DEPTH; k++)
+void update_forces() {
+    for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			for (int k = 0; k < GRID_DEPTH; k++) {
 				update_forces(i, j, k);
+			}
+		}
+	}
+}
 
-	/* User supplied hook, e.g. for adding custom forces (gravity, ...). */
-	if (inter_hook != NULL)
-		inter_hook();
-
-	for (i = 0; i < GRID_WIDTH; i++)
-		for (j = 0; j < GRID_HEIGHT; j++)
-			for (k = 0; k < GRID_DEPTH; k++)
+void update_particles() {
+    for (int i = 0; i < GRID_WIDTH; i++) {
+		for (int j = 0; j < GRID_HEIGHT; j++) {
+			for (int k = 0; k < GRID_DEPTH; k++) {
 				update_particles(i, j, k);
+			}
+		}
+	}
+}
 
-	/* User supplied hook, e.g. for handling collisions. */
-	if (post_hook != NULL)
+void update(void(*inter_hook)() = NULL, void(*post_hook)() = NULL) {
+    update_densities();
+    update_forces();
+
+    /* User supplied hook, e.g. for adding custom forces (gravity, ...). */
+	if (inter_hook != NULL) {
+		inter_hook();
+	}
+
+    update_particles();
+
+    /* User supplied hook, e.g. for handling collisions. */
+	if (post_hook != NULL) {
 		post_hook();
+	}
 
 	update_grid();
 }
 
-void init_particles(Particle *particles, int count)
-{
+void init_particles(Particle *particles, int count) {
 	grid = new GridElement[GRID_WIDTH * GRID_HEIGHT * GRID_DEPTH];
 	sleeping_grid = new GridElement[GRID_WIDTH * GRID_HEIGHT * GRID_DEPTH];
 
-	for (int x = 0; x < count; x++)
+	for (int x = 0; x < count; x++) {
 		ADD_TO_GRID(grid, particles[x]);
+	}
 }
 
